44514ca0a9cd38a0c82a7f56d28034a19fba2702
shruthik shetty <shrutikshetty14@gmail.com>
2024-12-21 18:57:13 +0530

    cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,14 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRouter from "./routes/users.mjs";
 import { logginMiddleware } from "./utils/middlewares.mjs";
-import productsRoute from "./routes/products.mjs";
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 // config dotenv to accept environment variables from .env file
 dotenv.config();
 
 // create an express app
 const app = express();
+
+//set up cookie parser
+app.use(cookieParser())
 
 //middleware to parse json bodies
 app.use(express.json());
@@ -27,6 +29,8 @@ const PORT = process.env.PORT ?? 3000;
 // create a route for the base url
 
 app.get("/", logginMiddleware, (req, res) => {
+  // time in mili sec
+  res.cookie("hello", "world",{maxAge: 60000*60})
   res.send("express fundamentals");
 });
 
