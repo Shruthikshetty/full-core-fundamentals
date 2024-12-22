201e85b776412254aab2db993b466d23983a5615
shruthik shetty <shrutikshetty14@gmail.com>
2024-12-22 14:16:20 +0530

    sessions 2

--- src/index.mjs
+++ src/index.mjs
@@ -4,6 +4,7 @@ import { logginMiddleware } from "./utils/middlewares.mjs";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 // config dotenv to accept environment variables from .env file
 dotenv.config();
@@ -53,6 +54,56 @@ app.get("/", logginMiddleware, (req, res) => {
   // time in mili sec
   res.cookie("hello", "world", { maxAge: 60000 * 60 });
   res.send("express fundamentals");
+});
+
+// authenticate password endpoint
+app.post("/api/auth", (req, res) => {
+  const { name, password } = req.body;
+
+  const findUser = mockUsers.find((user) => user.name === name);
+  if (!findUser || findUser.password !== password)
+    return res.status(401).send("authentication failed");
+
+  // modify session object to store as session cookie
+  req.session.user = findUser;
+
+  // Return user data without the password
+  const { password: _, ...userWithoutPassword } = findUser;
+  return res.status(200).send(userWithoutPassword);
+});
+
+// check if user is authenticated
+app.get("/api/auth/status", (req, res) => {
+  // checking the session store just for referece
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log("session store--->", session);
+  });
+
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(401).send({ msg: "not authenticated" });
+});
+
+// add item to cart works only if authenticated
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+  const { body: item } = req;
+
+  const { cart } = req.session;
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+
+  res.status(200).send(item);
+});
+
+// see the cart items
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) return res.sendStatus(401);
+
+  return res.send(req.session.cart ?? []);
 });
 
 // Start the Express server and listen for incoming requests on the specified PORT
